24019ff030fdb6038bf12ed4712b1d5c1913c62a
crazymoose77756 <ryanrogo064@gmail.com>
2025-01-19 13:17:38 -0500

    1.20.6 port

--- src/main/java/com/github/makubas/better_ladders/block/GoldLadderBlock.java
+++ src/main/java/com/github/makubas/better_ladders/block/GoldLadderBlock.java
@@ -1,9 +1,8 @@
 package com.github.makubas.better_ladders.block;
 
 import com.github.makubas.better_ladders.config.ConfigFile;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.LadderBlock;
-import net.minecraft.block.Material;
 import net.minecraft.sound.BlockSoundGroup;
 
 
@@ -12,7 +11,7 @@ public class GoldLadderBlock extends LadderBlock {
     public double ladderSpeed = ConfigFile.GOLD_LADDER_SPEED;
 
     public GoldLadderBlock() {
-        super(FabricBlockSettings.of(Material.METAL)
+        super(AbstractBlock.Settings.create()
                 .requiresTool()
                 .sounds(BlockSoundGroup.METAL)
                 .hardness(1.8F)
--- src/main/java/com/github/makubas/better_ladders/block/NetheriteLadderBlock.java
+++ src/main/java/com/github/makubas/better_ladders/block/NetheriteLadderBlock.java
@@ -1,9 +1,8 @@
 package com.github.makubas.better_ladders.block;
 
 import com.github.makubas.better_ladders.config.ConfigFile;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.LadderBlock;
-import net.minecraft.block.Material;
 import net.minecraft.sound.BlockSoundGroup;
 
 
@@ -12,7 +11,7 @@ public class NetheriteLadderBlock extends LadderBlock {
     public double ladderSpeed = ConfigFile.NETHERITE_LADDER_SPEED;
 
     public NetheriteLadderBlock() {
-        super(FabricBlockSettings.of(Material.METAL)
+        super(AbstractBlock.Settings.create()
                 .requiresTool()
                 .sounds(BlockSoundGroup.METAL)
                 .hardness(2.4F)
