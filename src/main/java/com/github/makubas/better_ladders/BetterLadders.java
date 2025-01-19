8ddcc1594d86dca2a338e811bf9ab1a880a6bf72
crazymoose77756 <ryanrogo064@gmail.com>
2025-01-19 13:03:47 -0500

    1.19.4 port

--- src/main/java/com/github/makubas/better_ladders/BetterLadders.java
+++ src/main/java/com/github/makubas/better_ladders/BetterLadders.java
@@ -1,40 +1,15 @@
 package com.github.makubas.better_ladders;
 
-import com.github.makubas.better_ladders.block.DiamondLadderBlock;
-import com.github.makubas.better_ladders.block.GoldLadderBlock;
-import com.github.makubas.better_ladders.block.IronLadderBlock;
-import com.github.makubas.better_ladders.block.NetheriteLadderBlock;
+import com.github.makubas.better_ladders.block.*;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-
 
 public class BetterLadders implements ModInitializer {
 
 	public static final String MOD_ID = "betlad";
 
-	private static final IronLadderBlock IRON_LADDER = new IronLadderBlock();
-	private static final GoldLadderBlock GOLD_LADDER = new GoldLadderBlock();
-	private static final DiamondLadderBlock DIAMOND_LADDER = new DiamondLadderBlock();
-	private static final NetheriteLadderBlock NETHERITE_LADDER = new NetheriteLadderBlock();
-
 	@Override
 	public void onInitialize() {
 		System.out.println("Better Ladders initialised!");
-
-		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "iron_ladder"), IRON_LADDER);
-		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "iron_ladder"), new BlockItem(IRON_LADDER, new FabricItemSettings().group(ItemGroup.MISC)));
-
-		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "gold_ladder"), GOLD_LADDER);
-		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "gold_ladder"), new BlockItem(GOLD_LADDER, new FabricItemSettings().group(ItemGroup.MISC)));
-
-		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "diamond_ladder"), DIAMOND_LADDER);
-		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "diamond_ladder"), new BlockItem(DIAMOND_LADDER, new FabricItemSettings().group(ItemGroup.MISC)));
-
-		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "netherite_ladder"), NETHERITE_LADDER);
-		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "netherite_ladder"), new BlockItem(NETHERITE_LADDER, new FabricItemSettings().group(ItemGroup.MISC)));
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/com/github/makubas/better_ladders/block/ModBlocks.java
@@ -0,0 +1,37 @@
+package com.github.makubas.better_ladders.block;
+
+import com.github.makubas.better_ladders.BetterLadders;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+
+    private static final Block IRON_LADDER =  registerBlock("iron_ladder", new IronLadderBlock());
+    private static final Block GOLD_LADDER = registerBlock("gold_ladder", new IronLadderBlock());
+    private static final Block DIAMOND_LADDER =registerBlock("diamond_ladder", new IronLadderBlock());
+    private static final Block NETHERITE_LADDER = registerBlock("netherite_ladder", new IronLadderBlock());
+
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, Identifier.of(BetterLadders.MOD_ID, name), block);
+    }
+
+    private static void registerBlockItem(String name, Block block) {
+        BlockItem item = new BlockItem(block, new Item.Settings());
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL)
+                .register((itemGroup) -> itemGroup.add(item));
+
+        Registry.register(Registries.ITEM, Identifier.of(BetterLadders.MOD_ID, name), item);
+    }
+
+    public static void registerModBlocks() {
+    }
+
+}
